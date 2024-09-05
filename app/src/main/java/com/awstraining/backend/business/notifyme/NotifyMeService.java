22e39fa7d7d5ed8ff21f895f59a4286e03c47fec
wojciech.bednarczyk <external.wojciech.bednarczyk@kuehne-nagel.com>
2024-09-05 14:09:33 +0200

    Lab1

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -1,23 +1,29 @@
 package com.awstraining.backend.business.notifyme;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotifyMeService {
 
+    private final MessageSender messageSender;
+//    private final Translator translator;
+//    private final Sentiment sentiment;
     // TODO: lab1
     //  1. Inject MessageSender.
     // TODO lab2
     //  1. Inject Translator
     // TODO lab3
     //  1. Inject sentiment detector
-//    @Autowired
-    public NotifyMeService() {
 
+    @Autowired
+    public NotifyMeService(MessageSender messageSender) {
+        this.messageSender = messageSender;
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
-      
+        var message = notifyMe.text();
+        messageSender.send(message);
         // TODO: lab1
         //  1. Send text using sender.
         //  2. Return sent message.
@@ -27,7 +33,7 @@ public class NotifyMeService {
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return "This service is not available.";
+        return message;
     }
     
 }
