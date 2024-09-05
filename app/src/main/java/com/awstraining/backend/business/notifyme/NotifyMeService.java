3ad1e538acfd3f8dc58847a42ac028ceea4258d7
wojciech.bednarczyk <external.wojciech.bednarczyk@kuehne-nagel.com>
2024-09-05 14:47:00 +0200

    lab2

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class NotifyMeService {
 
     private final MessageSender messageSender;
-//    private final Translator translator;
+    private final Translator translator;
 //    private final Sentiment sentiment;
     // TODO: lab1
     //  1. Inject MessageSender.
@@ -17,23 +17,25 @@ public class NotifyMeService {
     //  1. Inject sentiment detector
 
     @Autowired
-    public NotifyMeService(MessageSender messageSender) {
+    public NotifyMeService(MessageSender messageSender, Translator translator) {
         this.messageSender = messageSender;
+        this.translator = translator;
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
-        var message = notifyMe.text();
-        messageSender.send(message);
         // TODO: lab1
+        var message = notifyMe.text();
         //  1. Send text using sender.
         //  2. Return sent message.
         // TODO: lab2
         //  1. Translate text from using translator.
         //  2. Change sending of text to "translated text" and return it.
+        var translatedMessage = translator.translate(notifyMe);
+        messageSender.send(translatedMessage);
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return message;
+        return translatedMessage;
     }
     
 }
--- app/src/main/java/com/awstraining/backend/business/notifyme/adapter/TranslatorImpl.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/adapter/TranslatorImpl.java
@@ -1,32 +1,45 @@
 package com.awstraining.backend.business.notifyme.adapter;
 
+import com.amazonaws.services.translate.AmazonTranslate;
+import com.amazonaws.services.translate.model.TranslateTextRequest;
 import com.awstraining.backend.business.notifyme.NotifyMeDO;
 import com.awstraining.backend.business.notifyme.Translator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TranslatorImpl implements Translator {
 
     private static final Logger LOGGER = LogManager.getLogger(TranslatorImpl.class);
-    
+
+    private final AmazonTranslate amazonTranslate;
 
     
     // TODO: lab2
     //  1. Inject AWS AmazonTranslate from configuration TranslatorConfig.
-//    @Autowired
-    public TranslatorImpl() {
-        
+    @Autowired
+    public TranslatorImpl(AmazonTranslate amazonTranslate) {
+        this.amazonTranslate = amazonTranslate;
     }
     
     @Override
     public String translate(NotifyMeDO notifyMeDO) {
+        var translateTextRequest = new TranslateTextRequest();
+        translateTextRequest.setText(notifyMeDO.text());
+        translateTextRequest.setSourceLanguageCode(notifyMeDO.sourceLc());
+        translateTextRequest.setTargetLanguageCode(notifyMeDO.targetLc());
+
+        var result = amazonTranslate.translateText(translateTextRequest);
+
+        LOGGER.info("Translated text {}", result);
+
+        return result.getTranslatedText();
         // TODO: lab2
         //  1. Create translate text request.
         //  2. Call translate.
         //  3. Log information about successful translated message.
         //  4. Return translated message
-        return "";
     }
 }
